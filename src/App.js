6b10a0a9df78aeb444c45f12bff4cb0bd3b0d166
Nicolas A <nicolasalexandre0001@gmail.com>
2021-08-03 10:28:19 -0300

    feat:(logic):calculator simple operators logic

--- src/App.js
+++ src/App.js
@@ -3,22 +3,65 @@ import { StyleSheet, Text, View } from "react-native";
 import Button from "./components/Button";
 import Display from "./components/Display";
 
+const initialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
 const App = () => {
-  const [displayValue, setDisplayValue] = useState(0);
+  const [state, setState] = useState({ ...initialState });
 
   const addDigit = (number) => {
-    setDisplayValue(number);
+    
+    const clearDisplay = state.displayValue === "0" || state.clearDisplay;
+    if (number === "." && !clearDisplay && state.displayValue.includes(".")) {
+      return null;
+    }
+    const currentValue = clearDisplay ? "" : state.displayValue;
+    const displayValue = currentValue + number;
+    if (number !== ".") {
+      const newValue = parseFloat(displayValue);
+      const values = [...state.values];
+      values[state.current] = newValue;
+      setState({ ...state, values, displayValue, clearDisplay: false });
+    } else {
+      setState({ ...state, displayValue, clearDisplay: false });
+    }
   };
 
   const clearMemory = () => {
-    setDisplayValue(0);
+    setState(initialState);
   };
 
-  const setOperation = (operation) => {};
+  const setOperation = (operation) => {
+    if (state.current === 0) {
+      setState({ ...state, operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === "=";
+      const values = [...state.values];
+      try {
+        values[0] = eval(`${values[0]} ${state.operation} ${values[1]}`);
+      } catch (error) {
+        values[0] = state.values;
+      }
+      values[1] = 0;
+      setState({
+        ...state,
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
+    }
+  };
 
   return (
     <View style={style.container}>
-      <Display value={displayValue} />
+      <Display value={state.displayValue} />
       <View style={style.buttons}>
         <Button label="AC" triple onClick={clearMemory} />
         <Button label="/" operation onClick={setOperation} />
